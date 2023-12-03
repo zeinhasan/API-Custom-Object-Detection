b7125df14b54dbdfc523a4940da758b1679f9695
zeinhasan <ahmad.habib.hasan@mail.ugm.ac.id>
2023-12-04 00:34:06 +0700

    Upload model & Update API

---
+++ API/function.py
@@ -0,0 +1,50 @@
+import numpy as np
+import cv2
+import torch
+from google.cloud import storage
+from ultralytics import YOLO
+import json
+
+# Get the model from the bucket
+storage = storage.Client() # Initialize the client
+bucket = storage.bucket('object-detection-image') # Get the bucket
+blob = bucket.blob('Apple-YoloV8L.pt') # Get the model
+blob.download_to_filename('Apple-YoloV8L.pt') # Download the model
+model = YOLO('Apple-YoloV8L.pt') # Load the model
+
+def yolo(img):
+    # Assuming you have a YOLO model loaded
+    # Perform inference on the image
+    results = model.predict(img)
+    # Get the results in JSON format
+    result_json = results[0].tojson()
+    # Convert the JSON to a Python dictionary
+    result_json = json.loads(result_json)
+    # Get the annotated image
+    annotated_img = img.copy()
+    # Create an empty list to store the results
+    output_list = []
+    # Loop through the results and draw bounding boxes
+    for i, item in enumerate(result_json, 1):
+        label = item["name"]
+        confidence = item["confidence"]
+        box = item["box"]
+        x1, y1, x2, y2 = box["x1"], box["y1"], box["x2"], box["y2"]
+        # Convert the bounding box coordinates to integers
+        bbox = {
+            "x1": x1,
+            "y1": y1,
+            "x2": x2,
+            "y2": y2
+        }
+        # Append the results to the output list
+        output_list.append({
+            "name": label,
+            "bounding_box": bbox,
+            "confidence": confidence
+        })
+        # Draw bounding box on the image
+        cv2.rectangle(annotated_img, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
+        cv2.putText(annotated_img, f"{label} {confidence:.2f}", (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    # Return the results and annotated image
+    return output_list, annotated_img
---
+++ API/main.py
@@ -0,0 +1,28 @@
+import os
+import numpy as np
+from flask import Flask, render_template, request, jsonify
+import function
+import random
+import torch
+import cv2
+from io import BytesIO
+
+app=Flask(__name__)
+
+@app.route("/predict", methods=['GET', 'POST'])
+def predict(image):
+    if request.method == 'POST':
+        # Assuming the image is sent as a file with key 'image'
+        img_file = request.files['image']
+        img = cv2.imdecode(np.frombuffer(img_file.read(), np.uint8), cv2.IMREAD_COLOR)
+        img = cv2.resize(img, (416, 416))
+        # Run YOLO on the image and get annotated image
+        yolo_results, annotated_img = function.yolo(img)
+        # Convert annotated image to stream 
+        img_stream = cv2.imencode('.jpg', annotated_img)[1].tobytes()
+        # Return the results and annotated image as JSON
+        return jsonify({"results": yolo_results, "annotated_image": img_stream.decode('latin1')})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
